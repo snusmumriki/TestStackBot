21c792d8932e6f110b21b30f9e6788f4355a93f5
megaman <wdart1995@gmail.com>
2017-03-06 21:51:42 +0200

    kjhgkljhgl

--- teststackbot.py
+++ teststackbot.py
@@ -6,16 +6,23 @@ from flask.ext.redis import FlaskRedis
 bot = telebot.TeleBot('306447523:AAG4NEunw0OezXDbDjtTZMEFmRnLwYO5Yn8')
 
 app = Flask(__name__)
-app.config['REDIS_URL'] = 'redis://h:p0c08b0fb92a7de45ea5db298baf96d2f7bd48981912d73a19ec96ae3b2eb4634@ec2-34-251-82-220.eu-west-1.compute.amazonaws.com:7559'
+app.config[
+    'REDIS_URL'] = 'redis://h:p0c08b0fb92a7de45ea5db298baf96d2f7bd48981912d73a19ec96ae3b2eb4634@ec2-34-251-82-220.eu-west-1.compute.amazonaws.com:7559'
 redis = FlaskRedis(app)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    #bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    bot.send_message(message.chat.id, 'Hello, ' + message.from_user.first_name)
 
 
 @bot.message_handler(commands=['new'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(commands=['test'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
