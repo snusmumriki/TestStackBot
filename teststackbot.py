125e8e1d079bc8fe1a86d057c19e6eb4fa7215e1
megaman <wdart1995@gmail.com>
2017-03-07 12:56:09 +0200

    kjhgkljhgl

--- teststackbot.py
+++ teststackbot.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.config['REDIS_URL'] = 'redis://h:p0c08b0fb92a7de45ea5db298baf96d2f7bd48981912d73a19ec96ae3b2eb4634@ec2-34-251-82' \
                           '-220.eu-west-1.compute.amazonaws.com:7559'
 redis = FlaskRedis(app)
+temp = {}
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -24,51 +25,52 @@ def start(message):
 
 @bot.message_handler(commands=['new'])
 def new_test(message):
-    token = token_urlsafe(16)
+    token = token_urlsafe(8)
     redis.set(token, Test(token, []))
+
     msg = bot.send_message(message.chat.id, 'Set the questions count:')
-    msg.token = token
+    temp['token' + message.from_user.username] = token
     bot.register_next_step_handler(msg, set_units_num)
 
 
 def set_units_num(message):
     msg = bot.send_message(message.chat.id, 'Set the question text:')
-    msg.token = message.token
-    msg.num = int(message.text)
+    temp['num' + message.from_user.username] = int(message.text)
     bot.register_next_step_handler(msg, set_unit_text)
 
 
 def set_unit_text(message):
-    test = redis.get(message.token)
+    token = temp['token' + message.from_user.username]
+    test = redis.get(token)
     unit = Unit()
     unit.text = message.text
     test.units.append(unit)
-    redis.set(message.token, test)
+    redis.set(token, test)
 
     msg = bot.send_message(message.chat.id, 'Set the question answer:')
-    msg.token = message.token
-    msg.num = message.num
     bot.register_next_step_handler(msg, set_units_answer)
 
 
 def set_units_answer(message):
-    test = redis.get(message.token)
+    token = temp['token' + message.from_user.username]
+    num = temp['num' + message.from_user.username]
+
+    test = redis.get(token)
     test.units[-1].answer = message.text
-    redis.set(message.token, test)
-    if message.num > 1:
+    redis.set(token, test)
+
+    if num > 1:
         msg = bot.send_message(message.chat.id, 'Set the question text:')
         msg.num = message.num - 1
         bot.register_next_step_handler(msg, set_unit_text)
+    else:
+        del temp['num' + message.from_user.username]
+        del temp['token' + message.from_user.username]
 
 
 @bot.message_handler(commands=['test'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-'''@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)'''
 
 
 @app.route("/update", methods=['POST'])
