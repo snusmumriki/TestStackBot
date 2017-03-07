882b320dc53189608d8a42478b572563eeb3e826
megaman <wdart1995@gmail.com>
2017-03-07 12:42:16 +0200

    kjhgkljhgl

--- teststackbot.py
+++ teststackbot.py
@@ -66,9 +66,9 @@ def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+'''@bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)'''
 
 
 @app.route("/update", methods=['POST'])
