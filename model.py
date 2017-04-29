e6650c1c8249d4b8879d3ee76c9c171c32a4c964
megaman <wdart1995@gmail.com>
2017-04-29 22:12:11 +0200

    kjhgkljhgl

--- model.py
+++ model.py
@@ -1,26 +1,8 @@
-class Unit:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
+class Task:
+    text = None
+    answer = None
 
 
 class Test:
-    units = []
+    tasks = []
     results = {}
-
-    def __init__(self, token):
-        self.token = token
-
-
-class Draft:
-    def __init__(self, token, test, num):
-        self.token = token
-        self.test = test
-        self.num = num
-
-
-class Process:
-    def __init__(self, test, all, right):
-        self.test = test
-        self.all = all
-        self.right = right
--- teststack.py
+++ teststack.py
@@ -9,95 +9,96 @@ from flask.ext.redis import FlaskRedis
 
 from telebot.types import Update
 
-from model import Test, Unit, Draft
+from model import Test, Task, Draft
 
 bot = telebot.TeleBot('345467048:AAEFochiYcGcP7TD5JqYwco8E56cOYCydrk')
 
 app = Flask(__name__)
-app.config['REDIS_URL'] = 'redis://h:p0c08b0fb92a7de45ea5db298baf96d2f7bd48981912d73a19ec96ae3b2eb4634@ec2-34-251-82' \
-                          '-220.eu-west-1.compute.amazonaws.com:7559'
+app.config['REDIS_URL'] = 'redis://h:p0c08b0fb92a7de45ea5db298baf96d2f7bd48981912d73a19ec96ae3b2eb4634' \
+                          '@ec2-34-251-82-220.eu-west-1.compute.amazonaws.com:7559'
 redis = FlaskRedis(app)
-temp = {}
+tests = {}
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-    text = '''/new - create new test
-    /test - start test'''
+    text = '/new - create new test' \
+           '/pass - pass the test' \
+           '/mres - my result of the test' \
+           '/res - all results of the test'
     bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(commands=['new'])
 def new_test(message):
     try:
-        token = token_urlsafe(8)
-        temp['key'] = Draft(token, Test(token), None)
+        test = Test()
+        test.key = token_urlsafe(8)
+        tests['key'] = test
 
-        bot.send_message(message.chat.id, f'Your token: {token}')
-        msg = bot.send_message(message.chat.id, 'Set the questions count:')
-        bot.register_next_step_handler(msg, set_units_num)
+        bot.send_message(message.chat.id, f'Your key: {test.key}')
+        msg = bot.send_message(message.chat.id, 'Enter number of tasks')
+        bot.register_next_step_handler(msg, set_tasks_num)
     except Exception as e:
         bot.reply_to(message, str(e) + '0')
 
 
-def set_units_num(message):
+def set_tasks_num(message):
     try:
-        temp['key'].num = int(message.text)
-        msg = bot.send_message(message.chat.id, 'Set the question text:')
-        bot.register_next_step_handler(msg, set_unit_text)
+        tests['key'].num = int(message.text)
+        msg = bot.send_message(message.chat.id, 'Enter the task text')
+        bot.register_next_step_handler(msg, set_task_text)
     except Exception as e:
         bot.reply_to(message, str(e) + '1')
 
 
-def set_unit_text(message):
+def set_task_text(message):
     try:
-        test = temp['key'].test
-        test.units.append(Unit(message.text, None))
-
-        msg = bot.send_message(message.chat.id, 'Set the question answer:')
-        bot.register_next_step_handler(msg, set_units_answer)
+        task = Task()
+        task.text = message.text
+        tests['key'].tasks.append(task)
+        msg = bot.send_message(message.chat.id, 'Enter the task correct answer')
+        bot.register_next_step_handler(msg, set_task_answer)
     except Exception as e:
         bot.reply_to(message, str(e) + '2')
 
 
-def set_units_answer(message):
+def set_task_answer(message):
     try:
-        draft = temp['key']
-        test = draft.test
-        num = draft.num
-        test.units[-1].answer = message.text
+        test = tests['key']
+        test.tasks[-1].answer = message.text
 
-        if num > 1:
-            temp['key'].num = num - 1
-            msg = bot.send_message(message.chat.id, 'Set the question text:')
-            bot.register_next_step_handler(msg, set_unit_text)
+        if test.num > 1:
+            test.num -= 1
+            msg = bot.send_message(message.chat.id, 'Enter number of tasks')
+            bot.register_next_step_handler(msg, set_task_text)
         else:
-            del temp['key']
-            redis[draft.token] = pickle.dumps(test)
+            del tests['key']
+            redis[test.key] = pickle.dumps(test)
             bot.send_message(message.chat.id, 'Test successfully created!')
             li = []
-            for u in test.units:
+            for u in test.tasks:
                 li.append(f'{u.text} {u.answer}')
 
             bot.send_message(message.chat.id, str(li))
-
     except Exception as e:
         bot.reply_to(message, str(e) + '3')
 
 
-@bot.message_handler(commands=['test'])
-def start_test(message):
+@bot.message_handler(commands=['pass'])
+def begin(message):
     try:
-        msg = bot.send_message(message.chat.id, 'Enter the token')
-        bot.register_next_step_handler(msg, input_token)
+        msg = bot.send_message(message.chat.id, 'Enter the key')
+        bot.register_next_step_handler(msg, get_test)
     except Exception as e:
         bot.reply_to(message, str(e) + '0')
 
 
-def input_token(message):
+def get_test(message):
     try:
-        temp['key'] = redis[message.text]
-        msg = bot.send_message(message.chat.id, 'Set the question text:')
+        tests['key'] = redis[message.text]
+        num = len(tests['key'].tasks)
+        msg = bot.send_message(message.chat.id, f'Let\'s start the test, number of tasks: {num}')
         bot.register_next_step_handler(msg, set_unit_text)
     except Exception as e:
         bot.reply_to(message, str(e) + '1')
@@ -105,10 +106,10 @@ def input_token(message):
 
 def get_task(message):
     try:
-        test = temp['key']
+        test = tests['key']
         test.results['key'] = 0
-        test.count = len(test.units)
-        msg = bot.send_message(message.chat.id, test.units[0].text)
+        msg = bot.send_message(message.chat.id, 'Enter your answer')
+        msg = bot.send_message(message.chat.id, test.tasks[0].text)
         bot.register_next_step_handler(msg, set_unit_text)
     except Exception as e:
         bot.reply_to(message, str(e) + '1')
@@ -117,15 +118,15 @@ def get_task(message):
 def check_answer(message):
     try:
         key = message.from_user.username
-        test = temp['key']
-        units = test.units
-        test.num = len(units)
-        answer = units.pop(0).answer
+        test = tests['key']
+        tasks = test.tasks
+        test.num = len(tasks)
+        answer = tasks.pop(0).answer
         test.results['key'] += answer == message.text
-        if units:
+        if tasks:
             bot.register_next_step_handler(message, set_unit_text)
         else:
-            del temp['key']
+            del tests['key']
             redis.set('', pickle.dumps(test))
             bot.send_message(message.chat.id, f'Your result is: {test.results[key]}/{test.num}')
     except Exception as e:
@@ -133,7 +134,7 @@ def check_answer(message):
 
 
 @bot.message_handler(commands=['res'])
-def input_key_res(message):
+def input_key_result(message):
     try:
         msg = bot.send_message(message.chat.id, 'Enter the key')
         bot.register_next_step_handler(msg, get_result)
@@ -144,9 +145,30 @@ def input_key_res(message):
 @bot.message_handler(commands=['res'])
 def get_result(message):
     try:
-        key = message.from_user.username
-        test = temp['key']
-        bot.send_message(message.chat.id, f'Your result is: {test.results[key]}/{len(test.units)}')
+        test = tests['key']
+        result = test.results[message.from_user.username]
+        num = len(test.tasks)
+        bot.send_message(message.chat.id, f'Your result is: {result} / {num}')
+    except Exception as e:
+        bot.reply_to(message, str(e) + '0')
+
+
+@bot.message_handler(commands=['res'])
+def input_key_results(message):
+    try:
+        msg = bot.send_message(message.chat.id, 'Enter the key:')
+        bot.register_next_step_handler(msg, get_result)
+    except Exception as e:
+        bot.reply_to(message, str(e) + '0')
+
+
+@bot.message_handler(commands=['res'])
+def get_results(message):
+    try:
+        test = tests['key']
+        num = len(test.tasks)
+        results = test.results.items()
+        bot.send_message(message.chat.id, 'Results:\n'.join(f'{r[0]}: {r[1]} / {num}\n' for r in results))
     except Exception as e:
         bot.reply_to(message, str(e) + '0')
 
