3279cb96d77aa3c2f77b72574dfc8a04ca95b57e
megaman <wdart1995@gmail.com>
2017-04-30 17:34:45 +0200

    1

--- teststack.py
+++ teststack.py
@@ -2,7 +2,7 @@ from secrets import token_urlsafe
 
 import telebot
 from flask import Flask, request
-from redis import Redis
+from redis import StrictRedis
 
 from telebot.types import Update
 
@@ -11,10 +11,9 @@ from model import Test, Task
 bot = telebot.TeleBot('345467048:AAEFochiYcGcP7TD5JqYwco8E56cOYCydrk')
 
 app = Flask(__name__)
-redis = Redis(host='redis://h:p0c08b0fb92a7de45ea5db298baf96d2f7bd48981912d73a19ec96ae3b2eb4634' \
-                   '@ec2-34-251-82-220.eu-west-1.compute.amazonaws.com',
-              port='7559')
-Redis()
+redis = StrictRedis(host='redis://h:p0c08b0fb92a7de45ea5db298baf96d2f7bd48981912d73a19ec96ae3b2eb4634' \
+                         '@ec2-34-251-82-220.eu-west-1.compute.amazonaws.com',
+                    port='7559')
 tests = {}
 
 
